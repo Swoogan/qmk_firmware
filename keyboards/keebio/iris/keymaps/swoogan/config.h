8eab3cd9cd7a5c5379f370b9cf540a82f9d1bd21
Colin Svingen <swoogan@gmail.com>
2021-03-03 21:50:32 -0500

    Adds personal keymap

---
+++ keyboards/keebio/iris/keymaps/swoogan/config.h
@@ -0,0 +1,36 @@
+/*
+Copyright 2017 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// Hardware settings
+#define USE_SERIAL
+#define MASTER_LEFT
+
+// Home row mod settings
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+
+// Prevent normal rollover from triggering mods
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// This prevents accidental repeats of the tap-hold keys when typing quickly.
+#define TAPPING_FORCE_HOLD
+
+// This prevents short hold periods to be interpreted as individual taps when typing quickly.
+#define PERMISSIVE_HOLD
+
